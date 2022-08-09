235db996b16eb0a1f8ce6f882bf35dcb05c965eb
Indhiro <84686203+Indhiro@users.noreply.github.com>
2022-08-09 14:05:45 +0700

    Delete routers copy directory

--- routers copy/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const fileManageRouter = require('./fileMange.js');
-
-router.use('/file-manage', fileManageRouter);
-
-module.exports = router;
